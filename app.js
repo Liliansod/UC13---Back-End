dd17ba4306a47ca0dfdb4321f981832fb01ce121
Lilian <liliansod22@gmail.com>
2024-12-03 20:14:23 -0300

    Lilian

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-import carros2024 from './tabelacarro.js';
+import carros2024 from './tabelacarros.js';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (requisicao,resposta) => {
-    resposta.status(200).send(carros2024)
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(carros2024);
 });
 
-//iniciar servidor na porta 3000:
-app.listen(3000, () => console.log("Servidor rodando com sucesso"));
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carro) {
+        // ! Vazio Not
+        // Se o carro não for enctrado, retorna erro 404
+        resposta
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' //Mensagem de erro
+        );
+    return;
+    }
+    resposta.status(200).send(carro); // Se encontrado a sigla retorna a resposta correta
+});
+
+app.post('/', (req, res) => {
+    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
+    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
+});
+
+
+// Inicia o servidor na porta 3000:
+app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+
+// node app.js
+// localhost:3000/
